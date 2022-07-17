d071cc7997da6693878540d3dac6f215bce70491
Dmitriy <dimusgamer2.0@gmail.com>
2022-07-17 03:11:56 +0300

    Init files

---
+++ src/lib/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const Client = require('./Client');
+const Command = require('./Command');
+const Listener = require('./Listener');
+
+module.exports = { Client, Command, Listener };
